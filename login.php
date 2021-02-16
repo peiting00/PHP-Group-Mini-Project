68b8cd46f8532dec2c58654f0a3b2be8a030203d
Iris2000 <43512172+Iris2000@users.noreply.github.com>
2021-02-16 11:10:13 +0800

    login page redirection

--- login.php
+++ login.php
@@ -10,6 +10,8 @@
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="fonts/icomoon/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </head>
 
     <body>
@@ -17,9 +19,9 @@
             <img src="images/bg_1.jpg"/>
         </div>
         <div class="bottom">
+            <div class="alert" id="alert" role="alert">
+            </div>
             <div class="container col-md-6">
-                <div class="alert" id="alert" role="alert">
-                </div>
                 <div class="form-block">
                     <h3><strong>Login Page</strong></h3>
                     <form action="login.php" name="login" method="post">
@@ -33,12 +35,12 @@
                         </div>
                         <div class="form-group">
                             <label for="remember_me"><span>Remember me</span>
-                            <input type="checkbox" id="remember_me"/></label>
-                            <span><a href="#" class="forgot-pass">Forgot Password</a></span> 
+                            <input type="checkbox" name="remember_me" id="remember_me"/></label>
+                            <span><a href="resetPwd.php" class="forgot-pass">Forgot Password</a></span> 
                         </div>
                         <div class="form-group">
                             <span>New user?</span>
-                            <span><a href="#" class="create-acc">Create An Account</a></span> 
+                            <span><a href="createAcc.php" class="create-acc">Create An Account</a></span> 
                         </div>
                         <input type="submit" name="login" value="Log In" class="btn btn-block">
                     </form>
@@ -51,15 +53,17 @@
 <script type="text/javascript">
     function show_alert(status) {
         var alertDiv = document.getElementById("alert");
-        if (status == "success") {
-            alertDiv.classList.add("alert-success");
-            $(alertDiv).append("Login successfully.");
-        } else if (status == "fail_username") {
+
+        if (status == "fail_username") {
             alertDiv.classList.add("alert-warning");
             $(alertDiv).append("Username does not exists."); 
-        } else {
+        } else if (status == "fail_password") {
             alertDiv.classList.add("alert-danger");
             $(alertDiv).append("Wrong password."); 
+        } else {
+            alertDiv.classList.add("alert-success");
+            $(alertDiv).append("<button type='button' class='close' data-dismiss='alert'>&times;</button>"+
+                               "</button>Welcome, "+status+".");
         }
     }
 </script>
@@ -79,7 +83,8 @@
             if ($password_hash[0] == "") {
                 echo "<script>show_alert('fail_username')</script>";
             } else if (password_verify($password, $password_hash[0])) {
-                echo "<script>show_alert('success')</script>";
+                echo "<script>show_alert('$username')</script>";
+                header( "refresh:3;url=home.php" );
             } else {
                 echo "<script>show_alert('fail_password')</script>";
             }
