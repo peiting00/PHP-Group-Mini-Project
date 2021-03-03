1a03e463707817d362cc1838ada40effaf950a79
JosephLG4535 <78837456+JosephLG4535@users.noreply.github.com>
2021-03-03 12:10:22 +0800

    Update changePwd.php

--- changePwd.php
+++ changePwd.php
@@ -34,8 +34,17 @@
                             <input type="password" class="form-control" placeholder="Your Current Password" name="c_pwd" id="c_pwd" required>
                         </div>
                         <div class="form-group">
-                            <label for="password">New Password</label>
-                            <input type="password" class="form-control" placeholder="Your New Password" name="n_pwd" id="n_pwd" minlength="8" required>
+                        <label for="password">New Password <span style="color:red">*</span></label>
+                        <input type="password" class="form-control" id="password" name="n_pwd" placeholder="Your Password" pattern="(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" 
+                            onkeyup="keyUp()" required>
+                        </div>
+                        <div class="message" id="message" style="display:none" >
+                            <label style="font-weight:bold">Password must contain the following:</label>
+                            <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
+                            <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+                            <p id="number" class="invalid">A <b>number</b></p>
+                            <p id="length" class="invalid" >Minimum <b>8 characters</b></p>
                         </div>
                         <input type="reset" name="clear" value="Clear Input" class="btn btn-block">
                         <input type="submit" name="change_pwd" value="Change Password" class="btn btn-block">
@@ -48,9 +57,52 @@
 </html>
 
 <script type="text/javascript">
+    var myInput = document.getElementById("password");
+    var letter = document.getElementById("letter");
+    var capital = document.getElementById("capital");
+    var number = document.getElementById("number");
+    var length = document.getElementById("length");
+    myInput.onfocus = function() {
+        document.getElementById("message").style.display = "block";
+    }
+    myInput.onblur = function() {
+        document.getElementById("message").style.display = "none";
+    }
+    function keyUp() {
+        var lowerCaseLetters = /[a-z]/g;
+        if(myInput.value.match(lowerCaseLetters)) {  
+            letter.classList.remove("invalid");
+            letter.classList.add("valid");
+        } else {
+            letter.classList.remove("valid");
+            letter.classList.add("invalid");
+        }                   
+        var upperCaseLetters = /[A-Z]/g;
+        if(myInput.value.match(upperCaseLetters)) {  
+            capital.classList.remove("invalid");
+            capital.classList.add("valid");
+        } else {
+            capital.classList.remove("valid");
+            capital.classList.add("invalid");
+        }
+        var numbers = /[0-9]/g;
+        if(myInput.value.match(numbers)) {  
+            number.classList.remove("invalid");
+            number.classList.add("valid");
+        } else {
+            number.classList.remove("valid");
+            number.classList.add("invalid");
+        }            
+        if(myInput.value.length >= 8) {
+            length.classList.remove("invalid");
+            length.classList.add("valid");
+        } else {
+            length.classList.remove("valid");
+            length.classList.add("invalid");
+        }
+    }
     function show_alert(status) {
         var alertDiv = document.getElementById("alert");
-
         if (status == "fail_msg") {
             alertDiv.classList.add("alert-danger");
             $(alertDiv).append("Username and password combination does not exist. Your password remain unchanged."); 
@@ -67,10 +119,8 @@
         }
     }
 </script>
-
 <?php
     include "dbConnection.php";
-    session_start();
 
     if (isset($_POST["change_pwd"])) {
         $username = $_POST["username"];
